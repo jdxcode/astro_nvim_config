37b83fc156762768b512d9ef1ad4db3f8d6cdf0e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-20 11:35:52 -0500

    init

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,23 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    "copilot.lua",
+    opts = {},
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
